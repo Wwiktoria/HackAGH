46e648cb67e85bfaa0645490cdafc9a7cddfbe34
jakubzie <100830752+jakubzie@users.noreply.github.com>
2023-04-22 11:25:17 +0200

    first classes

---
+++ AutoClassLibrary/Car.cs
@@ -0,0 +1,29 @@
+public class Car
+{
+    public string vin;
+    public string regnum;
+    //jakiœ pseudonim dla samochodu
+
+    public double speed;
+    public bool frontlights;
+    public bool rearlights;
+    public bool doorsopen;
+
+    public string Vin { get { return vin; } set { vin = value; } }
+    public string Regnum { get { return regnum; } set { regnum = value; } }
+
+
+
+
+    public double SpeedCap()
+    {
+
+    }
+    public ToggleLights(bool lights)
+    {
+
+    }
+
+
+
+}
