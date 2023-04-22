669d2312fd6e9a8148ca683b607526405015159a
jakubzie <100830752+jakubzie@users.noreply.github.com>
2023-04-22 12:49:46 +0200

    User v.1

--- AutoClassLibrary/Car.cs
+++ AutoClassLibrary/Car.cs
@@ -3,26 +3,34 @@ public class Car
     public string vin;
     public string regnum;
     //jakiœ pseudonim dla samochodu
-
     public double speed;
     public bool frontlights;
     public bool rearlights;
     public bool doorsopen;
+    public (double, double) localization;
 
     public string Vin { get { return vin; } set { vin = value; } }
     public string Regnum { get { return regnum; } set { regnum = value; } }
-
-
+    public bool Frontlights { get { return frontlights; } set { frontlights = value; } }
+    public bool Rearlights { get { return rearlights; } set { rearlights = value; } }
+    public bool Doorsopen { get { return doorsopen; } set { doorsopen = value; } }
+    public (double, double) Localization { get { return localization; } set { localization = value; } }
 
 
     public double SpeedCap()
     {
 
     }
-    public ToggleLights(bool lights)
+    public bool ToggleLights(bool lights)
     {
 
     }
+    public ? ShowLocalization((double, double) localization)
+    {
+
+    }
+
+
 
 
 
